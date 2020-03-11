44958312936a092a034ebbf29542ccbe70777f90
Fabio Oliveira <fabio.braga@gmail.com>
2020-03-11 15:55:32 +0100

    Refactored script steps into bash shell scripts

--- scripts/terraform-plan.sh
+++ scripts/terraform-plan.sh
@@ -4,4 +4,4 @@ MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$MYDIR/terraform-setup.sh"
 
 terraform validate
-- terraform plan -out=tfplan -input=false
+terraform plan -out=tfplan -input=false
